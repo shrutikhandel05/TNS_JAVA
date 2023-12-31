700733d387824d40b90052ef9f67974e5f91a5c8
shrutikhandel05 <shrutikhandel05@gmail.com>
2023-12-31 12:09:12 +0530

    condition2 commited

---
+++ JAVA_TNS/src/com/condittional/condition2.java
@@ -0,0 +1,20 @@
+package com.condittional;
+import java.util.*;
+public class condition2 {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+	      System.out.println("Enter Age:6");
+	      int age = sc.nextInt();
+	      if (age < 18) {
+	         System.out.println("You are not eligible for voting");
+	      } else if (age >= 18) {
+	         System.out.println("You are  eligible for voting");
+	      } else {
+	         System.out.println("please enter valid age number!!");
+	      }
+
+
+	}
+
+}
